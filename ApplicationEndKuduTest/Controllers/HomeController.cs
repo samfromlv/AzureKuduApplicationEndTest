3f1dfdc5d9105996b0b2b1bf0e1c86b9775ea5eb
Alexander Shakhov <shakhov@yaklass.ru>
2018-11-06 10:23:29 +0200

    Initial commit

--- ApplicationEndKuduTest/Controllers/HomeController.cs
+++ ApplicationEndKuduTest/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +11,7 @@ namespace ApplicationEndKuduTest.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return Content("connectionStringValue=" + ConfigurationManager.ConnectionStrings["ApplicationEndKuduTest.Properties.Settings.MyConnectionString"] + "\r\nsettingValue=" + ConfigurationManager.AppSettings["MySetting"]);
         }
 
         public ActionResult About()
--- ApplicationEndKuduTest/Global.asax.cs
+++ ApplicationEndKuduTest/Global.asax.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,13 @@ namespace ApplicationEndKuduTest
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_End()
+        {
+            System.Diagnostics.Trace.TraceInformation("connectionStringValue="+ ConfigurationManager.ConnectionStrings["ApplicationEndKuduTest.Properties.Settings.MyConnectionString"]);
+            System.Diagnostics.Trace.TraceInformation("settingValue=" + ConfigurationManager.AppSettings["MySetting"]);
+            System.Diagnostics.Trace.Flush();
         }
     }
 }
